9344f96c96030020ea195d75a581d1357c7a5294
Flkalas <rectifying@gmail.com>
2015-07-24 23:08:50 +0900

    Video Test

--- src/configThreeLight.cpp
+++ src/configThreeLight.cpp
@@ -1,10 +1,49 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/videoio.hpp"
 #include <iostream>
+
+using namespace cv;
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl;
+int main(int argc, char* argv[])
+{
+    VideoCapture cap(-1); // open the video file for reading
 
-	cout << "Hello, Git!" << endl;
+    if ( !cap.isOpened() )  // if not success, exit program
+    {
+         cout << "Cannot open the video file" << endl;
+         return -1;
+    }
 
-	return 0;
+    //cap.set(CV_CAP_PROP_POS_MSEC, 300); //start the video at 300ms
+
+    double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
+
+     cout << "Frame per seconds : " << fps << endl;
+
+    namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+
+    while(1)
+    {
+        Mat frame;
+
+        bool bSuccess = cap.read(frame); // read a new frame from video
+
+         if (!bSuccess) //if not success, break loop
+        {
+                        cout << "Cannot read the frame from video file" << endl;
+                       break;
+        }
+
+        imshow("MyVideo", frame); //show the frame in "MyVideo" window
+
+        if(waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
+       {
+                cout << "esc key is pressed by user" << endl;
+                break;
+       }
+    }
+
+    return 0;
+
 }
